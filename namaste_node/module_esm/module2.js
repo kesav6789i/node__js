ba2873cfc1436ccf7d46efafb6a0cf42a06577ff
kesav6789i <126016617+kesav6789i@users.noreply.github.com>
2024-10-26 18:48:00 +0530

    learing about modules using esm

---
+++ namaste_node/module_esm/module2.js
@@ -0,0 +1,8 @@
+
+console.log('hi im module2');
+export var x = "travelled from module1";
+export function add(a,b)
+{
+    console.log(a+b);
+}
+
---
+++ namaste_node/module_esm/modules1.js
@@ -0,0 +1,10 @@
+
+import {x,add} from "./module2.js"
+console.log('im module1');
+let a=10;
+let b=10;
+// let add = 12;
+console.log('sum:'+ "  " + x + "  " +  add(a,b) );
+
+
+
