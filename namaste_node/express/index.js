b9c2334a4248e1900386e0f5b75b632d41f28fc9
kesav6789i <126016617+kesav6789i@users.noreply.github.com>
2024-11-18 14:32:57 +0530

    learnt abt express and nodemon

---
+++ namaste_node/express/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const path = require('path');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hi msg from server');
+
+})
+app.get('/hello', function (req, res) {
+  res.send('Hello.... msg from server');
+
+})
+
+
+
+app.listen(3000);
